8868130352eeccaddf12dbd542a0a4670ad94983
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-01-20 15:38:26 +0530

    feat: resume page

---
+++ src/app/resume/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="resume-wrapper">
+      <div className="h-screen">
+        <iframe
+          src="https://shanutiwari.me/resume/resume.pdf"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </main>
+  );
+}
