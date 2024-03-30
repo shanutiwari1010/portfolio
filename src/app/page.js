27febc6df10c379c5677d19015fe99dc3508192b
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-31 03:03:31 +0530

    refactor: change header component to hero component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
-import Header from "@/components/Header";
+"use client";
+
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <div style={{ backgroundColor: 'rgb(255, 247, 237)' }} className="w-full h-screen">
-      <Header />
+    <div
+      style={{ backgroundColor: "rgb(255, 247, 237)" }}
+      className="w-full h-screen"
+    >
+      <Hero />
     </div>
   );
 }
