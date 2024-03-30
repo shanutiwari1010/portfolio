d4f33dab4772668fef782289ba75f17d7e0c9fe4
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-30 21:45:12 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+// components/Header.js
+
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex justify-evenly bg-white p-4 items-center">
+      <nav>
+        <ul className="flex justify-center gap-10 bg-rgb(255,247,237)">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">Skills</Link>
+          </li>
+          <li>
+            <Link href="/about">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button className=" bg-orange-600 text-white px-4 py-2 hover:bg-blue-950 border border-none shadow-right shadow-bottom shadow-lg">Contact Now</button>
+    </header>
+  );
+};
+
+export default Header;
