e7ae3257642460c94f7cf9edea5ea0b8bae5580b
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-31 03:01:24 +0530

    chore: initialised hero component

---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+function Hero() {
+    return (
+      <div>
+        <h6>👋, My name is Shanu</h6>
+        <h1>
+          I'm a <spam></spam>
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Hero;
+  
