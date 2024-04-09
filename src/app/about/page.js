2cd3a79b37c8cc339ede23b89b0d84f29c4dca12
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-09 12:08:23 +0530

    update: added about section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,32 @@
+import Button from "@/components/Button";
+import { Contact } from "lucide-react";
+import React from "react";
 
-function About() {
+function page() {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <div
+      // style={{ backgroundColor: "rgb(255, 247, 237)" }}
+      className="h-screen flex items-center justify-between bg-white"
+    >
+      <div className="w-2/4	">
+        <img src="" alt="img" />
+      </div>
+      <div className="w-2/4 flex-col">
+        <h1 className=" text-4xl font-semibold">
+          I'm a Front-end Developer
+        </h1>
+        <p className="text-gray-600 text-lg max-w-xl pt-8">
+          I'm a Freelancer Front-end Developer. I'm from Indore,India. I code
+          and create web elements for amazing people around the world. I like
+          work with new people. New people new Experiences
+        </p>
+        <div className="flex gap-5">
+          <Button text="Contact me" path="contact" variant="primary"/>
+          <Button text="Projects" path="projects" variant="secondary"/>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default About;
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,20 @@
 
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
+import About from "@/app/about/page";
+import Skills from "./skills/page";
 
 export default function Home() {
   return (
     <div
       style={{ backgroundColor: "rgb(255, 247, 237)" }}
-      className="w-full h-screen pl-28"
+      className="w-full h-screen"
     >
       <Header />
       <Hero />
+      <About />
+      <Skills/>
+
     </div>
   );
 }
