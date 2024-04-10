5325959d574714f3d3d4e05cf6ca05ed8ac25bb0
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-11 02:19:07 +0530

    refactor: removed background color

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,14 @@
 
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
-
-import Skills from "@/app/skills/page";
+import Skills from "@/components/SkillCard";
 import Section from "@/components/Section";
 
 export default function Home() {
   return (
-    <div
-      style={{ backgroundColor: "rgb(255, 247, 237)" }}
-      className="w-full h-screen"
-    >
+    <div className="w-full h-screen max-w-screen-2xl ">
       <Header />
       <Hero />
-
       <Skills />
       <Section />
     </div>
