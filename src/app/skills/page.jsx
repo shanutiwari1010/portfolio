53adbee6cb2139acbbf87b188aff61f6d739785b
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-11 00:28:28 +0530

    refactor: removed background image

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -2,13 +2,7 @@ import SkillCard from "@/components/SkillCard/index";
 
 function Skills() {
   return (
-    <div
-      style={{
-        backgroundImage:
-          "url('https://uiocean.com/themeforest/react/krinky-tailwind/img/effect/bg-effect-1.png')",
-      }}
-      className="h-screen flex flex-col items-center justify-center"
-    >
+    <div className="h-screen flex flex-col items-center justify-center ">
       <div className="mb-8">
         <h1 className="uppercase text-4xl font-semibold">My Skills</h1>
         {/* <SkillCard color="bg-teal-200" logo="" text=""/> */}
@@ -17,7 +11,7 @@ function Skills() {
         <SkillCard />
       </div>
       <div className="">
-        <img src="" alt=""/>
+        <img src="" alt="" />
       </div>
     </div>
   );
