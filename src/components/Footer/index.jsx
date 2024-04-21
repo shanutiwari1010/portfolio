f3d57e98566686e7d62c707553e3e63ea987a96f
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-21 16:54:15 +0530

    style: change margin and padding

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,34 +6,13 @@ import { Linkedin, Github, Twitter, Instagram, Mail } from "lucide-react";
 function Footer() {
   return (
     <div className="flex flex-col justify-between px-20 pt-8 bg-orange-50 backdrop-blur-md ">
-     
       <div className="flex gap-4 justify-between items-center">
-        <div className="flex flex-col gap-2 items-center">
+        <div className="flex flex-col gap-4 items-center">
           <Link href="/">
-            <Image src="/assets/logo.svg" width={150} height={150} alt="logo" />
+            <Image src="/assets/logo.svg" width={110} height={110} alt="logo" />
           </Link>
-          <p className="text-gray-600 text-xs"> Thanks for popping in</p>
         </div>
-        {/* <div>
-          <ul className="flex flex-col gap-4 justify-center uppercase text-gray-600 text-sm">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-
-            <li>
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div> */}
-        <div className="flex flex-col gap-2 items-center">
-
-          <h1 className="text-gray-600 text-xs">Get in Touch</h1>
+        <div className="flex flex-col gap-4 items-center">
           <div className="flex">
             <Link
               href="https://www.linkedin.com/in/shanutiwari1010/"
@@ -68,7 +47,7 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="mb-3 text-sm text-center text-gray-600 dark:text-gray-400">
+      <div className=" pb-6 mt-10 text-sm text-center text-gray-600 dark:text-gray-400">
         Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
         <a href="https://web3templates.com/" target="_blank" rel="noopener">
           shanu
