045d3f05ba80463fa033373eb8c9d0001e9a82ea
Shanuti Tiwari <shanutiwari.work@gmail.com>
2025-04-16 00:00:12 +0530

    feat: added shadcn ui components

---
+++ src/components/ui/circle.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+import { cn } from "@/lib/utils";
+
+const Circle = forwardRef<
+  HTMLDivElement,
+  { className?: string; children?: React.ReactNode }
+>(({ className, children }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "z-10 flex size-20 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+});
+
+export { Circle };
+
+Circle.displayName = "Circle";
