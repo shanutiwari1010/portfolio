3ace40a7fde74adb1396e9b6e21998f58b7ffaa1
Shanuti Tiwari <shanutiwari.work@gmail.com>
2025-04-16 00:01:21 +0530

    feat: added constants

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export enum SOCIAL_URLS {
+  github = "https://github.com/shanutiwari1010",
+  linkedin = "https://linkedin.com/in/shanutiwari1010",
+  twitter = "https://x.com/shanutiwari1010",
+  meeting = "https://cal.com/shanutiwari1010",
+  mail = "mailto:shanutiwari.work@gmail.com",
+}
