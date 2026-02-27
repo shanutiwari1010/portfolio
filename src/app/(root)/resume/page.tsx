fdbc1ba62e9429f61bf491b66b6d189f855934f9
Shanu Tiwari <152061161+shanutiwari1010@users.noreply.github.com>
2026-02-27 19:36:58 +0530

    Update page.tsx

--- src/app/(root)/resume/page.tsx
+++ src/app/(root)/resume/page.tsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <section className="resume-wrapper">
       <div className="h-screen">
         <iframe
-          src="https://www.shanutiwari.com/resume/resume.pdf"
+          src="https://www.shanutiwari.in/resume/resume.pdf"
           width="100%"
           height="100%"
           title="Resume PDF"
